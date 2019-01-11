605c726ed6bce5d219e93e37cef633a3e5eea231
natemaia <natemaia10@gmail.com>
2019-01-11 15:47:53 -0800

    Update repo

--- x86_64/updaterepo.sh
+++ x86_64/updaterepo.sh
@@ -3,18 +3,23 @@
 # Repo update script using repo-add, rsync, and git
 # written by Nathaniel Maia for ArchLabs, December 2017
 
+SF_USER="$1"
+
 readonly RPATH="$(cd "$(dirname "$0")" || exit ; pwd -P)"
 readonly ARCHDIR="$(basename "$RPATH")"
 readonly REPO_PATH="$(sed "s~/${ARCHDIR}~~" <<< "$RPATH")"
+readonly SF_PATH="/home/frs/project/archlabs-repo/archlabs_repo/x86_64"
 
 if ! hash rsync git &>/dev/null; then
-    echo "ERROR: Script requires git installed"
+    echo "ERROR: Script requires both rsync and git installed"
     exit 1
 fi
 
 commit_repo() {
     cd "$REPO_PATH/$ARCHDIR" || return
+
     rm -f archlabs_repo.*
+
     repo-add archlabs_repo.db.tar.gz ./*.pkg.tar.xz
     rm -f archlabs_repo.db
     cp -f archlabs_repo.db.tar.gz archlabs_repo.db
@@ -33,10 +38,26 @@ commit_git() {
     fi
 }
 
+commit_sf() {
+    if [[ $SF_USER ]]; then
+        echo -e "\nUser: $SF_USER"
+        cd "$REPO_PATH/$ARCHDIR" || return
+        rsync -auvLPh -e ssh $(ls) "$SF_USER@frs.sourceforge.net:$SF_PATH"
+    else
+        printf "\nEnter Sourceforge Username: "
+        read -r SF_USER
+        commit_sf
+    fi
+}
+
 if [[ -d $REPO_PATH/$ARCHDIR ]]; then
     commit_repo
+
     echo -e "\nPushing to git origin"
     commit_git
+
+    echo -e "\nPushing to SF"
+    commit_sf
 else
     echo -e "\nCannot find repo directory: '$REPO_PATH/$ARCHDIR'"
     exit 1
